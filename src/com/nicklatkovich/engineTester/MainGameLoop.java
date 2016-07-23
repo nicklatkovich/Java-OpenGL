e6af606f4775b1f0320af2cbdadf1265608d0617
nicklatkovich <nicklatkovich@gmail.com>
2016-07-23 23:41:08 +0300

    VAOs and VBOs

--- src/com/nicklatkovich/engineTester/MainGameLoop.java
+++ src/com/nicklatkovich/engineTester/MainGameLoop.java
@@ -1,15 +1,34 @@
 package com.nicklatkovich.engineTester;
 
 import com.nicklatkovich.renderEngine.DisplayManager;
+import com.nicklatkovich.renderEngine.Loader;
+import com.nicklatkovich.renderEngine.RawModel;
+import com.nicklatkovich.renderEngine.Renderer;
 import org.lwjgl.opengl.Display;
 
 public class MainGameLoop {
 
     public static void main(String[] args) {
         DisplayManager.create();
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+        float[] vertices = {
+                // left bottom
+                -0.5f, 0.5f, 0f,
+                -0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f,
+                //right top
+                0.5f, -0.5f, 0f,
+                0.5f, 0.5f, 0f,
+                -0.5f, 0.5f, 0f
+        };
+        RawModel model = loader.loadToVAO(vertices);
         while (!Display.isCloseRequested()) {
+            renderer.prepare();
+            renderer.render(model);
             DisplayManager.update();
         }
+        loader.cleanUP();
         DisplayManager.destroy();
     }
 }
