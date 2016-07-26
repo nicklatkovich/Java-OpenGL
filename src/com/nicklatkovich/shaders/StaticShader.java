6cc76065329f37fdefe74840062b6343fa168572
nicklatkovich <nicklatkovich@gmail.com>
2016-07-26 21:35:16 +0300

    matrix and uniform variables

--- src/com/nicklatkovich/shaders/StaticShader.java
+++ src/com/nicklatkovich/shaders/StaticShader.java
@@ -1,9 +1,12 @@
 package com.nicklatkovich.shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgram {
 
     private static final String VERTEX_FILE = "src\\com\\nicklatkovich\\shaders\\vertexShader.txt";
     private static final String FRAGMENT_FILE = "src\\com\\nicklatkovich\\shaders\\fragmentShader";
+    private int location_transformationMatrix;
 
     public StaticShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
@@ -13,5 +16,14 @@ public class StaticShader extends ShaderProgram {
     protected void bindAttributes() {
         super.bindAttribute(0, "position");
         super.bindAttribute(1, "textureCoords");
+    }
+
+    @Override
+    protected void getAllUniformLocations() {
+        location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+    }
+    
+    public void loadTransformationMatrix(Matrix4f matrix) {
+        super.loadMatrix(location_transformationMatrix, matrix);
     }
 }
